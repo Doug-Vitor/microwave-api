2e15d24e3de25037991c8329eb3b4c61df81c41e
Douglas <douglasvitor.dev@gmail.com>
2025-06-14 12:23:58 -0300

    Database context

---
+++ src/Infra/WritableRepository.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+
+public class WritableRepository<T> : IWritableRepository<T> where T : BaseEntity
+{
+  public readonly DbSet<T> Collection;
+  protected readonly AppDbContext Context;
+
+  public WritableRepository(AppDbContext dbContext)
+    => (Context, Collection) = (dbContext, dbContext.Set<T>());
+
+  public async Task<T> InsertAsync(T entity)
+  {
+    await Collection.AddAsync(entity);
+    await Context.SaveChangesAsync();
+    return entity;
+  }
+
+  public async Task<T> UpdateAsync(int id, T entity)
+  {
+    entity.Id = id;
+    Context.Entry(entity).CurrentValues.SetValues(entity);
+
+    Context.Update(entity);
+    await Context.SaveChangesAsync();
+    return entity;
+  }
+}
