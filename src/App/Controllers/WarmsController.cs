5efb22ce3abf090a8e0dcc84849d85b16b142fe7
Douglas <douglasvitor.dev@gmail.com>
2025-06-14 17:26:24 -0300

    Adding validations

--- src/App/Controllers/WarmsController.cs
+++ src/App/Controllers/WarmsController.cs
@@ -2,23 +2,29 @@ using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 [Route("warms")]
-public class WarmsController : ControllerBase
+public class WarmsController(IWritableRepository<Warm> _repository) : ControllerBase
 {
-  private readonly IWritableRepository<Warm> _repository;
-
-  public WarmsController(IWritableRepository<Warm> repository) => _repository = repository;
-
   [HttpPost]
   public async Task<IActionResult> Post([FromBody] WarmDTO warm)
   {
-    var dbWarm = await _repository.InsertAsync(warm);
-    return Ok(dbWarm);
+    if (ModelState.IsValid)
+    {
+      var dbWarm = await _repository.InsertAsync(warm);
+      return Ok(dbWarm);
+    }
+
+    return BadRequest(ModelState);
   }
 
   [HttpPut("{id}")]
   public async Task<IActionResult> Put(int? id, [FromBody] WarmDTO warm)
   {
-    var dbWarm = await _repository.UpdateAsync(id.Value, warm);
-    return Ok(dbWarm);
+    if (ModelState.IsValid)
+    {
+      var dbWarm = await _repository.UpdateAsync(id.Value, warm);
+      return Ok(dbWarm);
+    }
+
+    return BadRequest(ModelState);
   }
 }
--- src/Core/DTOs/WarmDTO.cs
+++ src/Core/DTOs/WarmDTO.cs
@@ -1,7 +1,16 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 public class WarmDTO
 {
-  public int? Time { get; set; } = 30;
-  public int? Power { get; set; } = 10;
+  const int DEFAULT_TIME = 30;
+  const int DEFAULT_POWER = 10;
+
+  [Range(1, 120, ErrorMessage = "Tempo deve ser entre 1 e 120")]
+  public int? Time { get; set; } = DEFAULT_TIME;
+
+  [Range(1, 10, ErrorMessage = "Potência deve ser entre 1 e 10")]
+  public int? Power { get; set; } = DEFAULT_POWER;
 
   public WarmDTO() { }
 }
