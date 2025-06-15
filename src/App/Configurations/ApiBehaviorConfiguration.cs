721f88e4b8935c83935db311c4da758e785866f7
Douglas <douglasvitor.dev@gmail.com>
2025-06-15 15:45:34 -0300

    Configuring cors

---
+++ src/App/Configurations/ApiBehaviorConfiguration.cs
@@ -0,0 +1,18 @@
+internal static class ApiBehaviurConfiguration
+{
+  internal static IServiceCollection AddApiBehavior(this IServiceCollection services)
+  {
+    services.AddControllers();
+    services.ConfigureCors();
+    return services;
+  }
+
+  static IServiceCollection ConfigureCors(this IServiceCollection services)
+  {
+    services.AddCors(options =>
+      options.AddPolicy(Constants.LocalCors, policy => policy.WithOrigins("*"))
+    );
+
+    return services;
+  }
+}
